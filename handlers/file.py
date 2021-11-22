7cd0c80fc90a435a638e3ab1351fd02977d8eab0
lindaniele <lindaniele25@gmail.com>
2021-11-22 15:14:35 +0100

    修复bug

--- handlers/file.py
+++ handlers/file.py
@@ -17,11 +17,11 @@ async def load_dict():
     """
     Called when start_all. Loads json_files values.
     """
-    for file_name in [DATA_DIR+file for file in json_files]:
-        assert os.path.exists(file_name), f"{file_name} non trovato!"
-        assert os.path.getsize(file_name) > 0, f"{file_name} IS EMPTY!!"
+    for file_name in json_files:
+        # assert os.path.exists(file_name), f"{file_name} non trovato!"
+        # assert os.path.getsize(file_name) > 0, f"{file_name} IS EMPTY!!"
 
-        async with async_open(file_name, 'r') as json_file:
+        async with async_open(DATA_DIR+file_name, 'r') as json_file:
             print("loading.. ", file_name)
             file_content = await json_file.read()
             json_files[file_name] = json.loads(file_content)
@@ -34,8 +34,8 @@ async def write_json():
     Overwrites json files with the values in the dictionary storing its value every 5 minutes.
     """
     await asyncio.sleep(5*60)
-    for file_name in [DATA_DIR+file for file in json_files]:
-        async with async_open(file_name, 'w+') as json_file:
+    for file_name in json_files:
+        async with async_open(DATA_DIR+file_name, 'w+') as json_file:
             result = json.dumps(json_files[file_name], indent=4)
             await json_file.write(result)  # overwrite json with json_files values
 
